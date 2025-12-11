279b6424f59ab5163413fabb9f13a4cf741ffbd4
Eswar27 <antlasaieswar123@gmail.com>
2025-12-11 15:12:02 +0530

    third

--- email OTP.py
+++ email OTP.py
@@ -1,30 +1,36 @@
-#OTP generator
 import random
 import math
-import smtplib#simple mail transfered protocol libraries
+import smtplib  
 
-digits="0123456789@abcdefghijklmnopqrstuvwxyz"
-otp=""
+digits = "0123456789@abcdefghijklmnopqrstuvwxyz"
+otp = ""
 
 for i in range(6):
-    otp+=digits[math.floor(random.random()*10)]
-msg=otp
+    otp += digits[math.floor(random.random() * len(digits))]
 
-e=smtplib.SMTP("smtp.gmail.com",587)
+msg = otp
+
+e = smtplib.SMTP("smtp.gmail.com", 587)
 e.starttls()
-e.login("antlasaieswar123@gmail.com","luwf ozdl mhcb jntf")
-user="antlasaieswar123@gmail.com"
-emailid=input("enter the mail you want to send the otp:")
-e.sendmail(user,emailid,msg)
+e.login("antlasaieswar123@gmail.com", "luwf ozdl mhcb jntf")
+
+user = "antlasaieswar123@gmail.com"
+emailid = input("enter the mail you want to send the otp:")
+
+e.sendmail(user, emailid, msg)
 
 while True:
-    a=input("enter the otp:")
-    if a==otp:
+    a = input("enter the otp:")
+    if a == otp:
         print("otp is correct")
         break
-    elif a!=otp:
+    else:
         print("wrong otp")
-        e.sendmail(user,emailid,msg)
+        e = smtplib.SMTP("smtp.gmail.com", 587)
+        e.starttls()
+        e.login(user, "luwf ozdl mhcb jntf")
+        e.sendmail(user, emailid, msg)
+
         
             
     
